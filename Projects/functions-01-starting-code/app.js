439315dfa727c509e8f2c65cf35956e79cb61007
Codehoff <enno.kirchhoff1911@gmail.com>
2020-08-19 16:20:55 +0200

    player selection

--- Projects/functions-01-starting-code/app.js
+++ Projects/functions-01-starting-code/app.js
@@ -1,13 +1,32 @@
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = "ROCK";
+
+let gameisRunning = false;
+
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-    console.log("Game starting")
-}
-
-const person = {
-    greet: function greet() {
-        console.log("Hello There");
+const getPlayerChoice = function() {
+    const selection = prompt("Rock, Paper or Scissors?", "").toUpperCase();
+    if (
+        selection != ROCK && 
+        selection != SCISSORS && 
+        selection != PAPER
+    ){
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+        return DEFAULT_USER_CHOICE;
     }
+    return selection;
 };
 
-startGameBtn.addEventListener("click", startGame);
+startGameBtn.addEventListener("click", function() {
+    if (gameisRunning) {
+        return;
+    }
+
+    gameisRunning = true,
+    console.log("Game is starting...");
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
