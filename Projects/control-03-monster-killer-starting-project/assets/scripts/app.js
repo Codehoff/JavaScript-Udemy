70f88bc560e70ee3dd03819a6223122f69bc422d
Codehoff <enno.kirchhoff1911@gmail.com>
2020-08-19 13:20:24 +0200

    Heal Button

--- Projects/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Projects/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
-
+const STRONG_ATTACK_VALUE = 17;
+const HEAL_VALUE = 60;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -8,9 +9,19 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === "ATTACK") {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === "STRONG_ATTACK") {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    let damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
+    endRound();
+}
+
+function endRound() {
     const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= monsterDamage;
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
@@ -22,4 +33,21 @@ function attackHandler() {
     }
 }
 
+function attackHandler() {
+    attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+    attackMonster("STRONG_ATTACK");
+}
+
+function healPlayerHandler() {
+    increasePlayerHealth(HEAL_VALUE);
+    currentPlayerHealth += HEAL_VALUE;
+    endRound();
+}
+
+
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
