9a2d7729c9ea65dabd6fc33a2051bca26456fe0d
Codehoff <enno.kirchhoff1911@gmail.com>
2020-08-19 15:48:48 +0200

    functions

--- Projects/functions-01-starting-code/app.js
+++ Projects/functions-01-starting-code/app.js
@@ -1,1 +1,13 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log("Game starting")
+}
+
+const person = {
+    greet: function greet() {
+        console.log("Hello There");
+    }
+};
+
+startGameBtn.addEventListener("click", startGame);
