e234422225209b14b3cd060345266a264068f5ed
Codehoff <enno.kirchhoff1911@gmail.com>
2020-08-20 11:24:05 +0200

    Backdrop

--- Projects/dom-06-demo-project-setup/assets/scripts/app.js
+++ Projects/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,8 +1,13 @@
 const addMoviewModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
+const backdrop = document.getElementById("backdrop");
 
+const toggleBackdrop = () => {
+    backdrop.classList.toggle("visible");
+}
 
 startAddMovieButton.addEventListener("click", () => {
     addMoviewModal.classList.toggle("visible");
+    toggleBackdrop();
 });
 
