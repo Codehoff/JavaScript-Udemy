e3f58dc9cd13f416437d414599ce8f2c5235200c
Codehoff <enno.kirchhoff1911@gmail.com>
2020-08-20 11:33:24 +0200

    better backdrop ux

--- Projects/dom-06-demo-project-setup/assets/scripts/app.js
+++ Projects/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,6 +1,8 @@
 const addMoviewModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop");
+const cancelMovieAddButton = addMoviewModal.querySelector(".btn--passive");
+
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle("visible");
@@ -10,4 +12,18 @@ startAddMovieButton.addEventListener("click", () => {
     addMoviewModal.classList.toggle("visible");
     toggleBackdrop();
 });
+
+const backdropClickHandler = () => {
+    toggleBackdrop();
+    addMoviewModal.classList.toggle("visible");
+};
+
+backdrop.addEventListener("click", backdropClickHandler);
+
+const cancelAddMovie = () => {
+    addMoviewModal.classList.toggle("visible");
+    toggleBackdrop();
+};
+
+cancelMovieAddButton.addEventListener("click", cancelAddMovie)
 
